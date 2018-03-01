788d07c82e371b8efb3e2473d998c91171d4c904
jayesh sapkale <node.jayesh@gmail.com>
2018-03-01 12:27:11 +0530

    Test

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ var app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(express.static(path.join(__dirname, 'public')));
-    //app.locals.inspect = require('util').inspect;
+    app.locals.inspect = require('util').inspect;
 /*middleware*/
 
     // app.set('port', process.env.PORT);
